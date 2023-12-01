a1021e4ee0e86e50c023dfc6f1401500783a1d57
akmorningstar <134094289+akmorningstar@users.noreply.github.com>
2023-12-01 20:30:35 +0530

    Update sceneControl.java

--- 2022326_2022048/2022326_2022048/javafxtutorial/src/main/java/com/example/javafxtutorial/sceneControl.java
+++ 2022326_2022048/2022326_2022048/javafxtutorial/src/main/java/com/example/javafxtutorial/sceneControl.java
@@ -1,32 +1,36 @@
 package com.example.javafxtutorial;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
 import  javafx.scene.Parent;
 
-
+import java.io.IOException;
 
 
 public class sceneControl extends Application {
     private Game_controller gamecontrol;
+
     @Override
     public void start(Stage stage) throws Exception {
 
         try{
-            Parent root = FXMLLoader.load(getClass().getResource("scene2.fxml"));
+            FXMLLoader load =new FXMLLoader(getClass().getResource("scene2.fxml"));
+            Parent root = load.load();
+            load.setController(this);
             Scene scn = new Scene(root);
 
-            Image icon = new Image(getClass().getResource("/image/running1.png").toExternalForm());
-            stage.getIcons().add(icon);
             stage.setScene(scn);
+            stage.setResizable(false);
             stage.show();
         }
         catch (Exception e){
-            System.out.println(e);
+            e.printStackTrace();
         }
 
     }
