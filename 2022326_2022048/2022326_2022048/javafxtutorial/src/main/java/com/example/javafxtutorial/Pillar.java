7219cff88fbefb13fd82af396006a2845640831a
Nishantkumar22326 <132889056+Nishantkumar22326@users.noreply.github.com>
2023-12-10 19:03:45 +0530

    Update Pillar.java
    Pillar class updated.

--- 2022326_2022048/2022326_2022048/javafxtutorial/src/main/java/com/example/javafxtutorial/Pillar.java
+++ 2022326_2022048/2022326_2022048/javafxtutorial/src/main/java/com/example/javafxtutorial/Pillar.java
@@ -1,7 +1,38 @@
 package com.example.javafxtutorial;
 
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
+
 public class Pillar extends Basic_control {
+
+
+    public static ArrayList<Double> getDoubleList() {
+        return doubleList;
+    }
+
+    private static ArrayList<Double> doubleList = createRandomDoubleArrayList(37, 150);
+
+//    // Print the ArrayList
+//        System.out.println("Random Double ArrayList:");
+//        for (Double value : doubleList) {
+//        System.out.println(value);
+//    }
+
+
+    private static ArrayList<Double> createRandomDoubleArrayList(double minValue, double maxValue) {
+        ArrayList<Double> doubleList = new ArrayList<>();
+        Random random = new Random();
+
+        for (double i = minValue; i <= maxValue; i++) {
+            doubleList.add(i);
+        }
+
+        Collections.shuffle(doubleList, random);
+
+        return doubleList;
+    }
     private double width;
     private double height;
 
