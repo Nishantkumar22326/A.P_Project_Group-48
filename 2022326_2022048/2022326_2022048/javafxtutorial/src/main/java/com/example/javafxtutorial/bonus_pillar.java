7835267d253ef610248d531cae66792c0f82f280
Nishantkumar22326 <132889056+Nishantkumar22326@users.noreply.github.com>
2023-12-10 19:08:16 +0530

    Update bonus_pillar.java
    Bonus pillar class updated.

--- 2022326_2022048/2022326_2022048/javafxtutorial/src/main/java/com/example/javafxtutorial/bonus_pillar.java
+++ 2022326_2022048/2022326_2022048/javafxtutorial/src/main/java/com/example/javafxtutorial/bonus_pillar.java
@@ -1,12 +1,11 @@
 package com.example.javafxtutorial;
 
+
+// BONUS PILLAR CLASS HAVING VARIABLE WIDTH
 public class bonus_pillar extends Basic_control {
 
     private double width;
     private   double height= 50;
-
-
-
 
     public double getHeight() {
         return height;
@@ -23,6 +22,9 @@ public class bonus_pillar extends Basic_control {
     public void setWidth(double width) {
         this.width = width;
     }
+
+
+    // FUNCTION FOR DISPLAYING
     @Override
     public void display(){
 
