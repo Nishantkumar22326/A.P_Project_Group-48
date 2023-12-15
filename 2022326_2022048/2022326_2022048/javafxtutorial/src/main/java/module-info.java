f364a2dd0081f868e29c585d30fbb8f9509cb61e
akmorningstar <akash22048@iiitd.ac.in>
2023-12-15 18:45:03 +0530

    Increase button image

--- 2022326_2022048/2022326_2022048/javafxtutorial/src/main/java/module-info.java
+++ 2022326_2022048/2022326_2022048/javafxtutorial/src/main/java/module-info.java
@@ -2,6 +2,7 @@ module com.example.javafxtutorial {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.desktop;
+    requires org.testng;
 
 
     opens com.example.javafxtutorial to javafx.fxml;
