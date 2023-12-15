84939a469af62fedf677082196e0cc231f752a6e
akmorningstar <akash22048@iiitd.ac.in>
2023-12-15 18:25:34 +0530

    increase button image has been added

--- 2022326_2022048/2022326_2022048/javafxtutorial/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.javafxtutorial {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.javafxtutorial to javafx.fxml;
-    exports com.example.javafxtutorial;
-}
